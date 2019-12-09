57c5a1a38af65992f7547ddd58b12c136832235d
asmaafayed02 <asmaafayed02@gmail.com>
2019-12-09 17:04:21 +0200

    calculate result and validation

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ let billValue = document.getElementById('bill'),
     service = document.getElementById('service'),
      people = document.getElementById('people'),
      submitButton = document.getElementById('button'),
-     result = document.getElementById('result');
+     result = document.getElementById('result'),
+     resultSpan = document.getElementById('put');
 
 // hide the result 00.0
 result.style.display = 'none';
 // define the global or perant class
-     class items {
+   class items {
          bill
          ser
          number
@@ -22,38 +23,43 @@ result.style.display = 'none';
         //the child class 
         class Calculater extends items{
             constructor(bill,ser,number){
-                super(bill,ser,number);
+               super(bill,ser,number);
                 
             }
+        /*  bill
+          ser
+          number
+          constructor(bill,ser,number){
+               this.bill = bill;
+               this.ser = ser;
+               this.number = number;
+          }*/
             getInfo(){
-               // check the validation
-                if(this.bill = null|| this.number == null){
+               //check the validation
+               if(this.bill === ''|| this.number === ''){
                     window.alert('this field is required')
                    } 
                   
                 // calculate the value of amount tip
-                var calculateTip = ((this.bill * this.ser) / this.number);
+                var calculateTip = ((this.bill / this.ser) * this.number);
                 //for the float numbers
               //  calculateTip = Math.round(calculateTip * 100) / 100;
                 //show the result
-                result.style.display = 'block';
-                // put the result inside html element
-               result.textContent = calculateTip;
+          
+               return calculateTip;
                    
             }
         }
-        var serval = service.addEventListener('click',()=>{
-            let options = service.value;
-    
-               return options;
-              
-          })    
+     
        
         //claculate when the user click the button
     submitButton.addEventListener('click',(e)=>{
-         e.preventDefault();
-      
-        let calc = new Calculater(billValue.value,serval,people.value);
-        calc.getInfo()
-       
+         e.preventDefault();     
+        let calc = new Calculater(billValue.value,service.value,people.value);
+          var calculateTip = calc.getInfo();
+          
+       // put the result inside html element
+         resultSpan.innerHTML = calculateTip;
+         result.appendChild(resultSpan)
+         result.style.display = 'block';
     })
